12cea5eb258b11157d13273fbe2b641c37c0dae3
Ruslan <agabekovruslan322@gmail.com>
2026-04-19 01:34:47 +0300

    update bot

--- bot.py
+++ bot.py
@@ -8,14 +8,14 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 TOKEN = "8680262922:AAHNveyzRB_Gl4ZbFxq1JlceRXf8xQIeK3Q"
 
 from program import get_todays_goal, add_todays_goal
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def start(update: Update, context):
     text = (
         "I am your assistant 🚀\n\n"
         "Commands:\n"
         "/today - show todays goals\n"
         "/add - add todays goal\n"
     )
-    await update.message.reply_text("I am your assistant 🚀")
+    await update.message.reply_text(text)
 
 async def today(update, context):
     result = get_todays_goal()
