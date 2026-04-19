356620cb58a0534c0f40cbe1f13aecec3db9acd5
Ruslan <agabekovruslan322@gmail.com>
2026-04-19 11:40:31 +0300

    update bot

--- bot.py
+++ bot.py
@@ -41,15 +41,15 @@ async def add(update, context):
         except:
             await update.message.reply_text("File not found")
 
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
 
-app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("file", showfile))
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("today", today))
+    app.add_handler(CommandHandler("add", add))
 
-app.add_handler(CommandHandler("file", showfile))
-app.add_handler(CommandHandler("start", start))
-app.add_handler(CommandHandler("today", today))
-app.add_handler(CommandHandler("add", add))
-
-app.run_polling()
+    app.run_polling()
 
 if __name__ == "__main__":
     main()
