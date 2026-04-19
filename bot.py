ba36889bb768dc770135a1c5f85f561ed7584f3f
Ruslan <agabekovruslan322@gmail.com>
2026-04-19 10:57:20 +0300

    update bot

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from program import get_todays_goal, add_todays_goal
 
 TOKEN = "8680262922:AAHNveyzRB_Gl4ZbFxq1JlceRXf8xQIeK3Q"
 
-async def start(update: Update, context):
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = (
         "I am your assistant 🚀\n\n"
         "Commands:\n"
@@ -17,17 +17,31 @@ async def start(update: Update, context):
     )
     await update.message.reply_text(text)
 
-async def today(update, context):
+async def today(update, context: ContextTypes.DEFAULT_TYPE):
     result = get_todays_goal()
     await update.message.reply_text(result)
 
-async def add(update, context):
-    result = add_todays_goal()
+async def add(update, context: ContextTypes.DEFAULT_TYPE):
+    if not context.args:
+        await update.message.reply_text("Write goal after command.\nExample: /add Do homework")
+        return
+    
+    goal = " ".join(content.args)
+
+    result = add_todays_goal(goal)
     await update.message.reply_text(result)
+
+
+def main():
+    
 
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("today", today))
+app.add_handler(CommandHandler("add", add))
 
 app.run_polling()
+
+if __name__ == "__main__":
+    main()
