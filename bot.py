fa6593df863e9dd46a27aeaf3d2ba1b623f6baba
Ruslan <agabekovruslan322@gmail.com>
2026-04-21 23:18:09 +0300

    up bot.v2

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ async def add(update, context):
 
     await update.message.reply_text(result)
     
-async def list_goals(update, context):
+async def list_goals(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.message.from_user.id  
     result = show_goals(user_id)
     await update.message.reply_text("Список целей пуст!")
