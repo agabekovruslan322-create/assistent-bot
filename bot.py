d0aefa123f5cd3d2d074fe8c5d8dcd683abb8c0a
Ruslan <agabekovruslan322@gmail.com>
2026-05-07 04:37:40 +0300

    up.v2

--- bot.py
+++ bot.py
@@ -284,6 +284,8 @@ def main():
     app.add_handler(CallbackQueryHandler(button_handler))
 
     app.add_error_handler(error_handler)
-
     print("Synora запущен. Полет нормальный. 🚀")
     app.run_polling()
+
+if __name__ == "__main__":
+    main()
