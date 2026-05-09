43e3e6228efa3b351fddb510a21d36c60fbae8b7
Ruslan <agabekovruslan322@gmail.com>
2026-05-09 18:16:50 +0300

    up base

--- database.py
+++ database.py
@@ -9,13 +9,13 @@ def connect():
 def create_table():
     conn = connect()
     cursor = conn.cursor()
-
+    #Таблица клятв
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS vows (
             user_id BIGINT PRIMARY KEY
             )
         ''')
-
+    #ТАБЛИЦА ЦЕЛЕЙ
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS goals_v4 (
         id SERIAL PRIMARY KEY,
@@ -25,7 +25,7 @@ def create_table():
         is_completed BOOLEAN DEFAULT FALSE
     )
     """)
-    
+    #Таблица настроек
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS user_settings (
         user_id BIGINT PRIMARY KEY,
@@ -33,6 +33,11 @@ def create_table():
         timezone TEXT DEFAULT 'Europe/Moscow'
         )
     """)
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print("✅ Облачная база Synora v4 готова!")
 
 def get_or_create_settings(user_id):
     conn = connect()
@@ -42,14 +47,12 @@ def get_or_create_settings(user_id):
         INSERT INTO user_settings (user_id)
         VALUES (%s)
         ON CONFLICT (user_id) DO NOTHING;
-    """, (user_id))
+    """, (user_id,))
 
-    cursor.execute("SELECT day_end_time, FROM user_settings WHERE user_id = %s", (user_id,))
+    cursor.execute("SELECT day_end_time, timezone FROM user_settings WHERE user_id = %s", (user_id,))
     settings = cursor.fetchone()
+
     conn.commit()
+    cursor.close()
     conn.close()
     return settings
-
-    conn.commit()
-    conn.close()
-    print("Облачная база Synora v4 готова!")
