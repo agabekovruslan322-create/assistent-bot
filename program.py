034ddc1982bd3212e2cd4416118627ba1d988998
Ruslan <agabekovruslan322@gmail.com>
2026-05-16 00:32:41 +0300

    fix.v12 new fundament smart goal

--- program.py
+++ program.py
@@ -24,7 +24,7 @@ def add_todays_goal(user_id, text):
             goal_date,
             reminder_time
             ) 
-            VALUES (%s, %s, %s, %s, %s)
+            VALUES (%s, %s, %s, %s)
             """,
             (user_id, goal_date, text, reminder_time)
         )
